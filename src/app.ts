f89b11ce13e05eb5e3add49c3808eaddc7eca89b
profanis <fanis@projectbeagle.com>
2018-10-02 21:17:32 +0300

    dispatch actions

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,39 @@
-import { renderTodos } from './utils';
+import * as fromStore from "./store";
 
-const input = document.querySelector('input') as HTMLInputElement;
-const button = document.querySelector('button') as HTMLButtonElement;
-const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
-const todoList = document.querySelector('.todos') as HTMLLIElement;
+import { renderTodos } from "./utils";
+
+const input = document.querySelector("input") as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
+const todoList = document.querySelector(".todos") as HTMLLIElement;
+
+const store = new fromStore.Store(
+  {},
+  {
+    todos: [{ label: "pizaa", complete: false }]
+  }
+);
 
 button.addEventListener(
-  'click',
+  "click",
   () => {
     if (!input.value.trim()) return;
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    store.dispatch({
+      action: "ADD_TODO",
+      payload
+    })
 
-    input.value = '';
+    input.value = "";
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+todoList.addEventListener("click", function(event) {
   const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
+  if (target.nodeName.toLowerCase() === "button") {
     console.log(target);
   }
 });
